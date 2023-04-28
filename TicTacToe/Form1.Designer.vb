6c87b49f06faea55ad5b31c8c90aee0e9cbb1a1f
HadMil <131375061+HadMil@users.noreply.github.com>
2023-04-28 17:06:44 +0100

    Add files via upload

---
+++ TicTacToe/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.reset = New System.Windows.Forms.Button()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Silver
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Silver
+        Me.Button1.Location = New System.Drawing.Point(226, 89)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 100)
+        Me.Button1.TabIndex = 0
+        Me.Button1.TabStop = False
+        Me.Button1.Text = " "
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Silver
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Silver
+        Me.Button2.Location = New System.Drawing.Point(332, 89)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 100)
+        Me.Button2.TabIndex = 1
+        Me.Button2.TabStop = False
+        Me.Button2.Text = " "
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Silver
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Silver
+        Me.Button3.Location = New System.Drawing.Point(438, 89)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 100)
+        Me.Button3.TabIndex = 2
+        Me.Button3.TabStop = False
+        Me.Button3.Text = " "
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Silver
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.Silver
+        Me.Button4.Location = New System.Drawing.Point(226, 195)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 100)
+        Me.Button4.TabIndex = 3
+        Me.Button4.TabStop = False
+        Me.Button4.Text = " "
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Silver
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Silver
+        Me.Button5.Location = New System.Drawing.Point(332, 195)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 100)
+        Me.Button5.TabIndex = 4
+        Me.Button5.TabStop = False
+        Me.Button5.Text = " "
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Silver
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.Color.Silver
+        Me.Button6.Location = New System.Drawing.Point(438, 195)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 100)
+        Me.Button6.TabIndex = 5
+        Me.Button6.TabStop = False
+        Me.Button6.Text = " "
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Silver
+        Me.Button7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.Color.Silver
+        Me.Button7.Location = New System.Drawing.Point(226, 301)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 100)
+        Me.Button7.TabIndex = 6
+        Me.Button7.TabStop = False
+        Me.Button7.Text = " "
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Silver
+        Me.Button8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.ForeColor = System.Drawing.Color.Silver
+        Me.Button8.Location = New System.Drawing.Point(332, 301)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(100, 100)
+        Me.Button8.TabIndex = 7
+        Me.Button8.TabStop = False
+        Me.Button8.Text = " "
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Silver
+        Me.Button9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Font = New System.Drawing.Font("Arial Black", 27.84906!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.ForeColor = System.Drawing.Color.Silver
+        Me.Button9.Location = New System.Drawing.Point(438, 301)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(100, 100)
+        Me.Button9.TabIndex = 8
+        Me.Button9.TabStop = False
+        Me.Button9.Text = " "
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'reset
+        '
+        Me.reset.BackColor = System.Drawing.Color.Silver
+        Me.reset.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.reset.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.69811!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.reset.ForeColor = System.Drawing.Color.Black
+        Me.reset.Location = New System.Drawing.Point(226, 464)
+        Me.reset.Name = "reset"
+        Me.reset.Size = New System.Drawing.Size(312, 100)
+        Me.reset.TabIndex = 9
+        Me.reset.TabStop = False
+        Me.reset.Text = "Reset"
+        Me.reset.UseVisualStyleBackColor = False
+        '
+        'Timer
+        '
+        Me.Timer.Enabled = True
+        Me.Timer.Interval = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(774, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.ClientSize = New System.Drawing.Size(774, 592)
+        Me.Controls.Add(Me.reset)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Tick Tack Toe"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents reset As Button
+    Friend WithEvents Timer As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ TicTacToe/Form1.vb
@@ -0,0 +1,191 @@
+﻿Public Class Form1
+    Dim turn As Integer
+    Sub New()
+        InitializeComponent()
+        'Setup the game board ready to start
+        InitialiseGameBoard()
+    End Sub
+
+    ''' <summary>
+    ''' Handles when a game button has been selected
+    ''' </summary>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button4.Click, Button5.Click, Button6.Click, Button7.Click, Button8.Click, Button9.Click
+
+        Dim ButtonSelected As Button = CType(sender, Button)
+        'Set the vlaues for this button when it is selected
+        SetupTurnValues(ButtonSelected)
+        'Check to see if the game has been won
+        CheckMoveForWin(ButtonSelected)
+    End Sub
+
+    ''' <summary>
+    ''' Handles when the user selects to restart the game again the game again
+    ''' </summary>
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles reset.Click
+        'Setup the game board ready to start again
+        InitialiseGameBoard()
+    End Sub
+
+    ''' <summary>
+    ''' Initialises the game board ready to start the game
+    ''' </summary>
+    Private Sub InitialiseGameBoard()
+        Button1.Text = " "
+        Button2.Text = " "
+        Button3.Text = " "
+        Button4.Text = " "
+        Button5.Text = " "
+        Button6.Text = " "
+        Button7.Text = " "
+        Button8.Text = " "
+        Button9.Text = " "
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+        turn = 1
+        Me.BackColor = Color.Red
+    End Sub
+
+    ''' <summary>
+    ''' Time to check to see if the game has ended in a draw.
+    ''' </summary>
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        Me.Timer.Enabled = False
+        'If we have got to this turn and no one has won then we can assume it's a draw.
+        If turn = 9 Then
+            MsgBox("Draw")
+            turn += 1
+        End If
+        Me.Timer.Enabled = True
+    End Sub
+
+    ''' <summary>
+    ''' Set the text for the button selected, disable the button from being selected and what background colour needs to be next.
+    ''' </summary>
+    Private Sub SetupTurnValues(ButtonSelected As Button)
+        'Set the button text and background colour for the next go 
+        If turn Mod 2 = 1 Then
+            ButtonSelected.Text = "X"
+            Me.BackColor = Color.Blue
+        Else
+            ButtonSelected.Text = "O"
+            Me.BackColor = Color.Red
+        End If
+        'Update the turn counter and disable the button
+        turn += 1
+        ButtonSelected.Enabled = False
+    End Sub
+
+    ''' <summary>
+    ''' Check to see if that last move has won the game 
+    ''' </summary>
+    Private Sub CheckMoveForWin(ButtonSelected As Button)
+
+        Dim GameWon As Boolean = False
+
+        Select Case ButtonSelected.Name
+            Case "Button1"
+                If Button1.Text = Button2.Text AndAlso Button1.Text = Button3.Text Then
+                    GameWon = True
+                ElseIf Button1.Text = Button4.Text AndAlso Button1.Text = Button7.Text Then
+                    GameWon = True
+                ElseIf Button1.Text = Button5.Text AndAlso Button1.Text = Button9.Text Then
+                    GameWon = True
+                End If
+            Case "Button2"
+                If Button1.Text = Button2.Text AndAlso Button1.Text = Button3.Text Then
+                    GameWon = True
+                ElseIf Button2.Text = Button5.Text AndAlso Button2.Text = Button8.Text Then
+                    GameWon = True
+                End If
+            Case "Button3"
+                If Button1.Text = Button2.Text AndAlso Button1.Text = Button3.Text Then
+                    GameWon = True
+                ElseIf Button3.Text = Button6.Text AndAlso Button3.Text = Button9.Text Then
+                    GameWon = True
+                ElseIf Button3.Text = Button5.Text AndAlso Button3.Text = Button7.Text Then
+                    GameWon = True
+                End If
+            Case "Button4"
+                If Button4.Text = Button5.Text AndAlso Button4.Text = Button6.Text Then
+                    GameWon = True
+                ElseIf Button1.Text = Button4.Text AndAlso Button1.Text = Button7.Text Then
+                    GameWon = True
+                End If
+            Case "Button5"
+                If Button4.Text = Button5.Text AndAlso Button4.Text = Button6.Text Then
+                    GameWon = True
+                ElseIf Button2.Text = Button5.Text AndAlso Button2.Text = Button8.Text Then
+                    GameWon = True
+                ElseIf Button1.Text = Button5.Text AndAlso Button1.Text = Button9.Text Then
+                    GameWon = True
+                ElseIf Button3.Text = Button5.Text AndAlso Button3.Text = Button7.Text Then
+                    GameWon = True
+                End If
+            Case "Button6"
+                If Button4.Text = Button5.Text And Button4.Text = Button6.Text Then
+                    GameWon = True
+                ElseIf Button3.Text = Button6.Text And Button3.Text = Button9.Text Then
+                    GameWon = True
+                End If
+            Case "Button7"
+                If Button1.Text = Button4.Text AndAlso Button1.Text = Button7.Text Then
+                    GameWon = True
+                ElseIf Button3.Text = Button5.Text AndAlso Button3.Text = Button7.Text Then
+                    GameWon = True
+                ElseIf Button7.Text = Button8.Text AndAlso Button7.Text = Button9.Text Then
+                    GameWon = True
+                End If
+            Case "Button8"
+                If Button7.Text = Button8.Text AndAlso Button7.Text = Button9.Text Then
+                    GameWon = True
+                ElseIf Button2.Text = Button5.Text AndAlso Button2.Text = Button8.Text Then
+                    GameWon = True
+                End If
+            Case "Button9"
+                If Button7.Text = Button8.Text AndAlso Button7.Text = Button9.Text Then
+                    GameWon = True
+                ElseIf Button1.Text = Button5.Text AndAlso Button1.Text = Button9.Text Then
+                    GameWon = True
+                ElseIf Button3.Text = Button6.Text AndAlso Button3.Text = Button9.Text Then
+                    GameWon = True
+                End If
+        End Select
+
+        'The move has won the game so show the message and disable the board
+        If (GameWon) Then
+            MsgBox(" '" & ButtonSelected.Text & "' Wins")
+            DisableBoard()
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Disable all the buttons for the game
+    ''' </summary>
+    Private Sub DisableBoard()
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+
+        Dim msg As String = "This implementation of Tic Tac Toe was developed using VB.Net in Visual Studio 2019, enjoy."
+        MessageBox.Show(msg, "About", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+
+End Class
